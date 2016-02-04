269a2abc2a17e18fa44df68944dbdce71f0aeb51
Haaruun <haaruun.dhubat@outlook.com>
2016-02-04 12:32:53 -0500

    Blah

--- main.js
+++ main.js
@@ -1,26 +1,90 @@
+// 
 $(function(){
-	$('.button').hover(function(){
-		$(this).toggleClass('turn');
-	});
 
-	
 
-	$('.button').click(function(){
-		var video = document.getElementById('video');
-		if(video.paused){
- 		video.play();
- 		$('h1').addClass('op');
- 		$('.in').html('Pause');
+var Video = document.getElementById('Video');
+var playPause = document.getElementById('playPause');    
+var Seek = document.getElementById('inputSeek');
+//var mute = document.getElementById('mute');
+var Volume = document.getElementById('inputVolume');
+//var fullScreen = document.getElementById('fullscreen');    
+
+
+
+
+  $('#playPause').click(function(){
+       if (Video.paused){
+            Video.play();
+            $('#playPause').html('| |');
+        }else{
+            Video.pause();
+            $('#playPause').html('▶');
+        }        
+});
+
+    
+   var muteBtn = document.getElementById('mute');
+muteBtn.addEventListener('click', function(){
+	if(Video.muted == false){
+		Video.muted = true;
+        $('#mute').html('<i class="fa fa-volume-off"></i>');
 	}else{
-		video.pause();
-		$('h1').removeClass('op');
-		$('.in').html('Play');
+		Video.muted = false;
+        $('#mute').html('<i class="fa fa-volume-up"></i>');
+
 	}
+});
+    
+    inputVolume.addEventListener("change", function(){
+        Video.volume = inputVolume.value;
+    });
+    
+    
+    
+    
+// var fullScreen = document.getElementById('fullscreen');
+// fullScreen.addEventListener('click', function(){
+// 	if(Video.requestFullscreen){
+// 		Video.requestFullscreen();
+//         $('main').addClass('full');
+//          $('.con').addClass('maincon');
+// 	}else if (Video.mozRequestFullScreen) {
+//     Video.mozRequestFullScreen(); // Firefox
+//         $('main').addClass('full');
+//          $('.con').addClass('maincon');
+//   } else if (Video.webkitRequestFullscreen) {
+//     Video.webkitRequestFullscreen(); // Chrome and Safari
+//        $('.con').addClass('maincon');
+//   }
+// });
+    
+    
+    
+    Video.addEventListener('timeupdate', function(){
+        var pct = Video.currentTime / Video.duration * 100;
+        Seek.value = pct;
+         
+        Video.onended = function(e) {
+        $('#playPause').html('▶');
+           Seek.value = 0;
+        };
+        
+        
+    });
+    
+    
+    Seek.addEventListener('change', function(){
+        var videoTime = Seek.value * Video.duration / 100;
+        Video.currentTime = videoTime;
+    });
+      
+  
 
-	});
+     
 
 
 
-	
 
+
+  
 });
